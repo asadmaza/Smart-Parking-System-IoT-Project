0ef8b9a8844d655cb532b006e26a580ad90c0303
Asad <asadmaza@hotmail.com>
2023-09-10 19:56:06 +0800

    Updated Simulated Test.py - will now connect and publish messages to IoT Core, still need to figure out database

--- 5. Connection and Transmission/mqtt pubsub test.py
+++ 5. Connection and Transmission/mqtt pubsub test.py
@@ -1,6 +1,7 @@
 """
 Test file to ensure there is a two-way connection to a Cloud
-Platform - AWS IoT Core to RPI.
+Platform - AWS IoT Core to RPI using MQTT Protocol and
+Publish and Subscribe.
 
 Components required:
 x1 Raspberry Pi Kit (RPI, cables, power source, breadboard etc.)
@@ -8,6 +9,7 @@ x1 Raspberry Pi Kit (RPI, cables, power source, breadboard etc.)
 Code description:
 Test file to test whether I can send and receive data from
 AWS IoT core before it gets complicated with RPI and sensors.
+Can both send and receive messages, DEBUG mode enabled.
 --------------------------------------------------------------------
 Setup:
 
@@ -18,16 +20,22 @@ Setup AWS IoT Core:
 2. Install AWSIoTPythonSDK, using terminal:
 pip install AWSIoTPythonSDK
 Successfully installed AWSIoTPythonSDK-1.5.2
+
+Configure the topics for subscribe and publish accordingly.
 --------------------------------------------------------------------
 
 Date: 09/09/2023
 Author: Asad Maza - Group 3
 """
-
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import time
 import json
 import random
+import logging
+
+# Enable logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
@@ -36,28 +44,45 @@ def customCallback(client, userdata, message):
     print("from topic: ")
     print(message.topic)
 
-# Configure MQTT client
-myMQTTClient = AWSIoTMQTTClient("RaspberryPi-Asad")  # Things device name
-myMQTTClient.configureEndpoint("a2mqsm6nqkmbh6-ats.iot.ap-southeast-2.amazonaws.com", 8883)
-myMQTTClient.configureCredentials("/home/asadRPI/Desktop/AWS IoT Core/root-CA.crt",
-                                  "/home/asadRPI/Desktop/AWS IoT Core/private.pem.key",
-                                  "/home/asadRPI/Desktop/AWS IoT Core")
+try:
+    # Configure MQTT client
+    myMQTTClient = AWSIoTMQTTClient("basicPubSub") # Using working sample in SDK folder
+    myMQTTClient.configureEndpoint("a2mqsm6nqkmbh6-ats.iot.ap-southeast-2.amazonaws.com", 8883)
 
-# Connect to AWS IoT
-myMQTTClient.connect()
+    # Setup credentials
+    myMQTTClient.configureCredentials("/home/asadRPI/Desktop/AWS IoT Core/root-CA.crt",
+                                      "/home/asadRPI/Desktop/AWS IoT Core/RaspberryPi-Asad.private.key",
+                                      "/home/asadRPI/Desktop/AWS IoT Core/RaspberryPi-Asad.cert.pem")
 
-# Subscribe to a topic, ensure you have setup on AWS IoT Core correctly
-print("Subscribing to topic 'sdk/test/AWS'") # 
-myMQTTClient.subscribe("sdk/test/AWS", 1, customCallback)
-time.sleep(2)  # Wait to ensure the subscription is active
+    # Add additional configurations to align with working example
+    myMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+    myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+    myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-# Some test codePublish to the same topic in a loop forever
-while True:
-    message = {
-        'message': 'Hello from RaspberryPi!',
-        'value': random.randint(0, 100)
-    }
-    print(f"Publishing message to topic 'sdk/test/Python': {json.dumps(message)}")
-    myMQTTClient.publish("my/test/topic", json.dumps(message), 0)
-    time.sleep(5)  # Publish every 10 seconds
-    
+    # Connect to AWS IoT
+    if myMQTTClient.connect():
+        print("Connected successfully!")
+    else:
+        print("Could not connect. Exiting.")
+        exit(1)
+
+    # Subscribe topic
+    print("Subscribing to topic 'sdk/test/python'")
+    myMQTTClient.subscribe("sdk/test/python", 1, customCallback)
+    time.sleep(2)  # Wait to ensure the subscription is active
+
+    # Test Code: Publish to the same topic in a loop forever
+    while True:
+        message = {
+            'message': 'Hello from RaspberryPi!',
+            'value': random.randint(0, 100)
+        }
+        # Publish topic
+        print(f"Publishing message to topic 'sdk/test/python': {json.dumps(message)}")
+        myMQTTClient.publish("sdk/test/python", json.dumps(message), 1)  # Changed QoS to 1
+        time.sleep(5)  # Publish every 5 seconds
+
+except Exception as e:
+    print(f"An exception occurred: {e}")
