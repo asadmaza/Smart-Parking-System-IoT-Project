e86885cc931817edbdbae53c966cb7d941e5b054
wuryantoAji <ajiw4056@gmail.com>
2023-09-19 12:35:19 +0800

    done with scheduler

--- README.md
+++ README.md
@@ -3,18 +3,29 @@ Repository for Group 3 Course CITS5506 2023 Second Semester
 
 ## To Do List
 ### raspberry Script
-    - adjust the input from manual typing to read from a sensor
+    - make a v4 of asad's local script that actually 
+        1. publish to the aws iot core topic
+        2. subscribe to the aws iot core topic
+        3. init the bay states by publish a message to the topic and waiting for the appropriate response
 
 ### web application    
-    - Create a function to subscribe from mqtt aws iot core and change the state of the webapp(sqlite.db) whenever a car is entering and exiting the parking bay and publish back as a feedback from webapp
+    - Create a function to subscribe from mqtt aws iot core and change the state of the webapp(sqlite.db) whenever a car is entering and exiting the parking bay and publish back as a feedback from webapp (WIP)
     - Create a function to publish to mqtt aws iot core whenever a parking bay is reserved from the webapp and subscribe to check for feedback from the raspberry pi
-    - Create a scheduler function that runs every minute to check if there's an expiring reserved bay and publish to raspberry pi
+    - Create a scheduler function that runs every minute to check if there's an expiring reserved bay and publish to raspberry pi (DONE)
+    - Implement function to give the initial state bay from the database to the raspberry pi through by publishing to the topic through aws iot core mqtt
     
     -- Nice to Have --
     - Create input validation for the booking form
     - Change the web UI to accommodate for camera streaming on the situation of the parking bay
     - Send email to customer whenever a parking bay is reserved from the webapp
     - Apply best practice such as separate config
+
+### Potential Improvement
+#### Web application side
+    - safeguard from racing condition
+    - implement payment method
+    - user account implementation
+    
 
 ## How to run
 1. create a new virtual environment
@@ -37,3 +48,18 @@ Repository for Group 3 Course CITS5506 2023 Second Semester
     sqlite3 mockIoTDB.db < initdb.sql 
 4. run to populate the mock data to the database
     cat insertToDetail.sql | sqlite3 mockIoTDB.db
+
+## How to contribute
+If you are interested to do one of the function that's in the web application section please do this first
+1. make a new branch from this aji_branch or main branch if this is already merged
+    git checkout -b "<insert branch name>" - format the branch name like yourName_what_you_are_trying_to_do
+2. make your changes
+3. test it
+4. stage and commit your work
+    git add . -> do this in the Group3...... folder
+    git commit -m "<insert commit message>"
+5. push to your branch
+    git push origin <branch_name>
+6. create a pull request
+
+# Feel free to DM if you have anything to ask
--- webApplication/index.py
+++ webApplication/index.py
@@ -1,17 +1,58 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 from sqlalchemy import select, create_engine, update
 import os
+import json
 from datetime import datetime, date
 
 file_path = os.path.abspath(os.getcwd())+"/databaseStruct/mockIoT.db"
 
 db = SQLAlchemy()
+scheduler = APScheduler()
+myMQTTClient = AWSIoTMQTTClient("aji_laptop")
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + file_path
+
+# Topic const
+INIT_BAY_STATE = "INIT_BAY_STATE"
+BAY_STATUS_CHANGE_FROM_DEVICE = "BAY_STATUS_CHANGE_FROM_DEVICE"
+FEEDBACK_TO_DEVICE_FROM_CHANGING_STATUS = "FEEDBACK_TO_DEVICE_FROM_CHANGING_STATUS"
+SEND_BAY_CHANGE_STATUS_WHEN_RESERVED = "SEND_BAY_CHANGE_STATUS_WHEN_RESERVED"
+FEEDBACK_FROM_DEVICE_WHEN_BAY_IS_RESERVED = "FEEDBACK_FROM_DEVICE_WHEN_BAY_IS_RESERVED"
+
+# Global Variable for waiting state
+feedbackWaitingControl = False
+
+def customCallback(client, userdata, message):
+    if(message.topic == BAY_STATUS_CHANGE_FROM_DEVICE):
+        print("Received a bay status change from device : ")
+        print(message.payload.decode("utf-8"))
+        statusChangeFromDevice(message.payload.decode("utf-8"))
+    if(message.topic == FEEDBACK_FROM_DEVICE_WHEN_BAY_IS_RESERVED):
+        print("Received a feedback message from device : ")
+        print(message.payload.decode("utf-8"))
+        # To-Do -> change the feedbackWaitingControl to True
+    if(message.topic == INIT_BAY_STATE):
+        print("Received a feedback message from device : ")
+        print(message.payload.decode("utf-8"))
+        giveinitialBayState()
+
+# AWS IoT client setup
+
+myMQTTClient.configureEndpoint("a30y98prchbi0n-ats.iot.us-west-2.amazonaws.com", 8883)
+myMQTTClient.configureCredentials(os.path.abspath(os.getcwd())+"/aws-certif/root-CA.crt",
+                                      os.path.abspath(os.getcwd())+"/aws-certif/aji_laptop.private.key",
+                                      os.path.abspath(os.getcwd())+"/aws-certif/aji_laptop.cert.pem")
+
+# Confirm MQTT Connection
+myMQTTClient.connect()
+myMQTTClient.subscribe(BAY_STATUS_CHANGE_FROM_DEVICE, 1, customCallback)
+myMQTTClient.subscribe(FEEDBACK_FROM_DEVICE_WHEN_BAY_IS_RESERVED, 1, customCallback)
+myMQTTClient.subscribe(INIT_BAY_STATE, 1, customCallback)
+
 db.init_app(app)
-scheduler = APScheduler()
 scheduler.api_enabled = True
 scheduler.init_app(app)
 scheduler.start()
@@ -44,6 +85,7 @@ class BookingParkingBay(db.Model):
     plate_number = db.Column(db.String, nullable=False)
     booking_entry_time = db.Column(db.DateTime, nullable=False)
     booking_exit_time = db.Column(db.DateTime, nullable=False)
+    is_expired = db.Column(db.Integer, nullable=False)
 
 @app.route("/home")
 def homePage():
@@ -86,9 +128,9 @@ def bookingForm(parking_bay):
         timeOut = request.form['tOut']
 
         print("a bay is being reserved")
-        # publish message to indicate a parking bay is reserved
+        # To-Do -> publish message to indicate a parking bay is reserved (To-Do)
 
-        # wait for a feedback from the raspberry pi via subscriber 
+        # To-Do -> wait for a feedback from the raspberry pi via subscriber by doing a while loop the condition is in the feedbackWaitingControl in global variable, 
 
         getParkingBayDetail = select(ParkingBayDetail).where(ParkingBayDetail.parking_bay_name==parking_bay)
         queryResult = createConnectionAndExecuteQuery(getParkingBayDetail)
@@ -108,12 +150,10 @@ def bookingForm(parking_bay):
                 phone_number = phoneNumber,
                 plate_number = plateNumber,
                 booking_entry_time = entryDate,
-                booking_exit_time = exitDate)
+                booking_exit_time = exitDate,
+                is_expired = 0)
         db.session.add(entry)
-        
-        # except do rollback
-    
-        # finally commit  
+
         db.session.commit() 
         
         result = {
@@ -128,13 +168,97 @@ def bookingForm(parking_bay):
         
         return render_template('bookingFormSuccess.html', result=result)  
 
+# function for checking the db each minutes to check for expiring reservation but the bay is not filled(status=1)
+@scheduler.task('cron', id='resettingExpiredBay', minute='*')
+def resettingExpiredBay():
+    with scheduler.app.app_context():
+        getBookingDetail = select(BookingParkingBay).where(BookingParkingBay.booking_exit_time<datetime.now()).where(BookingParkingBay.is_expired==0)
+        queryResultBookingDetail = createConnectionAndExecuteQuery(getBookingDetail)
+        for row in queryResultBookingDetail:
+                bookingId = int(row.rowid)
+                parkingBayId = row.parking_bay_id
+                fullName = row.full_name
+                email = row.email
+                phoneNumber = row.phone_number
+                plateNumber = row.plate_number
+                bookingEntryTime = row.booking_entry_time
+                bookingExitTime = row.booking_exit_time
+                isExpired = row.is_expired
+                
+                updateExpiredParkingBayDetail = ParkingBayDetail.query.filter_by(rowid=parkingBayId).first()
+                updateExpiredParkingBayDetail.parking_bay_status = 0
+
+                updateExpiredBookingDetail = BookingParkingBay.query.filter_by(rowid=bookingId).first()
+                updateExpiredBookingDetail.is_expired = 1
+
+                db.session.commit()
+
+    return redirect("/",200)
+
 # function to subscribe the mqtt topic and change the status accordingly (when a car is entering a valid bay, when a car is exiting a non-reserved bay, 
 # when a car is entering a reserved bay, when a car is exiting a reserved bay)
+def statusChangeFromDevice(messagePayload):
+    print(messagePayload)
+    getParkingBayDetail = select(ParkingBayDetail).where(ParkingBayDetail.parking_bay_name==parkingName) #change to actual variable from the message payload
+    queryResult = createConnectionAndExecuteQuery(getParkingBayDetail)
+    for row in queryResult:
+        parkingBayId = int(row.rowid)
+        parkingName = row.parking_bay_name
+    if(statusChange == 0): #change to actual variable from the message payload
+        print("car is exiting parking bay")
+        latestParkingBayTimestamp = select(ParkingBayTimestamp).where(ParkingBayTimestamp.parking_bay_id==parkingBayId).order_by(ParkingBayTimestamp.parking_bay_entry_time.desc()).limit(1)
+        executeQueryFindLatestParkingStamp = createConnectionAndExecuteQuery(latestParkingBayTimestamp)
+        #create exception when no data is found
+        for row in executeQueryFindLatestParkingStamp:
+            timeStampRowid = row.rowid
+            parkingBayId = row.parking_bay_id
+            entryTime = row.parking_bay_entry_time
+        exitTime = timestamp
+        totalDuration = exitTime-entryTime
+        secondsInDay = 24 * 60 * 60
+        totalMinutes = (totalDuration.days * secondsInDay + totalDuration.seconds) / 60
 
-# function for checking the db each minutes to check for expiring reservation but the bay is not filled(status=1)
+        updateParkingBayTimestampQuery = ParkingBayTimestamp.query.filter_by(rowid=timeStampRowid).first()
+        updateParkingBayTimestampQuery.parking_bay_exit_time = exitTime
+        updateParkingBayTimestampQuery.parking_bay_total_minutes = totalMinutes
 
+        updateParkingBayDetailQuery = ParkingBayDetail.query.filter_by(rowid=parkingBayId).first()
+        updateParkingBayDetailQuery.parking_bay_status = statusChange
+        
+        db.session.commit()
+
+    elif(statusChange == 1): #change to actual variable from the message payload
+        print("car is entering parking bay")
+        updateParkingBayDetailQuery = ParkingBayDetail.query.filter_by(rowid=parkingBayId).first()
+        
+        if(updateParkingBayDetailQuery.parking_bay_status == 2):
+            updateParkingBayDetailQuery.parking_bay_status = statusChange
+            updateBookingDetail = BookingParkingBay.query.filter_by(BookingParkingBay.parking_bay_id==parkingBayId).filter_by(BookingParkingBay.is_expired==0).first()
+            updateBookingDetail.is_expired = 1
+        
+        elif(updateParkingBayDetailQuery.parking_bay_status == 0):
+            updateParkingBayDetailQuery.parking_bay_status = statusChange
+
+        entry = ParkingBayTimestamp(parking_bay_id = parkingBayId, parking_bay_entry_time = timestamp)
+        db.session.add(entry)
+        
+        db.session.commit() 
+
+    return redirect("/",200)
+
+def giveinitialBayState():
+    # To-Do -> implement this function
+    # query and get all the parking bay detail
+    # publish to the raspberry pi
+    return redirect("/",200) 
 
 # later deprecate all of below
+@app.route("/publish_test")
+def publishTest():
+    print('Begin Publish')
+    myMQTTClient.publish(TOPIC, json.dumps({"message":MESSAGE}), 1)
+    return "<p>nice one</p>"
+
 @app.route("/")
 def index():
     arrayOfParkingBay = []
@@ -229,6 +353,10 @@ def statusChange():
         return '<h1>you are not allowed to access this page by this method</h1>'
 
 if __name__ == '__main__':
-    # run app in debug mode on port 5000
-    # configure for publisher and subsrciber
-    app.run(debug=True, port=5000)
+    try:
+        app.run(debug=True, port=5000)
+
+    except KeyboardInterrupt:
+        print("Terminating and cleaning up")
+        db.session.close()
+        myMQTTClient.disconnect()
